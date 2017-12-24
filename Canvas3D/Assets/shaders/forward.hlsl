5d7d52503598df3c24822ab7776448c2f92c1565
Warty <ItzWarty@gmail.com>
2017-12-24 14:36:50 -0800

    Still not much

--- Canvas3D/Assets/shaders/forward.hlsl
+++ Canvas3D/Assets/shaders/forward.hlsl
@@ -49,6 +49,8 @@ float4 PSMain(PSInput input) : SV_TARGET {
    float3 base = baseAndTransparency.xyz;
    float transparency = baseAndTransparency.w;
 
+   float3 colorAccumulator = 0;
+
    [branch] if (PBR_ENABLED) {
       float metallic, roughness;
       pbrMaterialProperties(input.positionWorld, metallic, roughness);
@@ -61,40 +63,36 @@ float4 PSMain(PSInput input) : SV_TARGET {
       /**/
       float nDotV = dot(N, V);
 
-      float3 colorAccumulator = 0;
       for (uint i = 0; i != numSpotlights; i++) {
          colorAccumulator += pbrComputeSpotlightDirectContribution(P, N, i, V, nDotV, diffuse, F0, roughness);
       }
 
       // environment lighting
-      //{
-      //   // L is V reflection based on normal.
-      //   float3 L = -V + 2.0f * dot(V, N) * N;
-      //   float3 unattenuatedLightContribution = pbrComputeUnattenuatedLightContribution(P, N, L, V, nDotV, diffuse, F0, roughness);
-      //   colorAccumulator += unattenuatedLightContribution * 0.05f;
-      //}
+      if (false)
+      {
+         // L is V reflection based on normal.
+         float3 L = -V + 2.0f * dot(V, N) * N;
+         float3 unattenuatedLightContribution = pbrComputeUnattenuatedLightContribution(P, N, L, V, nDotV, diffuse, F0, roughness);
+         colorAccumulator += unattenuatedLightContribution * 0.05f;
+      }
 
       // Don't support variable exposure yet, so just hardcoded this.
-      colorAccumulator *= 1.0f;
+      colorAccumulator *= 4.0f;
 
       // Monitor gamma decodes with x^2.2, so invert that here (gamma encoding/expansion)
       // to achieve linear colorspace. Alternatively could use sRGB backbuffer colorspace.
       // http://www.codinglabs.net/article_gamma_vs_linear.aspx
-      return float4(pow(colorAccumulator, 1.0f / 2.2f), transparency);
    } else {
-      // Traditional diffuse lighting
-      float4 diffuse = input.color * SampleDiffuseMap(input.uv, input.positionObject.xyz, input.normalObject.xyz);
-
-      float4 colorAccumulator = float4(0, 0, 0, diffuse.w);
+      // Traditional diffuse lighting (diffuse is base)
       if (!shadowTestEnabled) {
-         colorAccumulator = diffuse;
-      }
-      else {
+         colorAccumulator = base;
+      } else {
          for (uint i = 0; i != numSpotlights; i++) {
             float3 lighting = computeSpotlightLighting(input.positionWorld, input.normalWorld, ShadowMaps, SpotlightDescriptions[i]);
-            colorAccumulator += diffuse * float4(lighting, 0);
+            colorAccumulator += base * lighting;
          }
       }
-      return colorAccumulator;
    }
+
+   return float4(pow(colorAccumulator, 1.0f / 2.2f), transparency);
 }
