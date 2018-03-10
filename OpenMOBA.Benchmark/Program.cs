0f53ee6d646d7caac180922b2c4f41a68c9bbbc7
Michael Yu <miyu@dargon.io>
2018-03-10 04:06:37 -0800

    Wip benchmarking

--- OpenMOBA.Benchmark/Program.cs
+++ OpenMOBA.Benchmark/Program.cs
@@ -1,19 +1,43 @@
-﻿using System.Numerics;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Horology;
+using BenchmarkDotNet.Jobs;
 using OpenMOBA.Foundation;
 using OpenMOBA.Foundation.Terrain;
 using OpenMOBA.Geometry;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
 
 namespace OpenMOBA.Benchmark {
    public class Program {
       public static void Main(string[] args) {
-         BenchmarkRunner.Run<HolePunch3DBenchmark>();
+         var benchmark = new HolePunch3DBenchmark();
+         benchmark._ClearMapAndLoadBunny();
+         var sw = new Stopwatch();
+         sw.Start();
+         for (var i = 0; i < 10; i++) {
+            benchmark._InvalidateCaches();
+            benchmark._CompileBunny();
+
+            var ton = benchmark.terrainService.CompileSnapshot().OverlayNetworkManager.CompileTerrainOverlayNetwork(15);
+            var terrainNodes = ton.TerrainNodes.ToArray();
+            for (var j = 0; j < terrainNodes.Length; j++) {
+               Console.WriteLine(terrainNodes[j].SectorNodeDescription);
+            }
+            Console.WriteLine(terrainNodes.Length);
+         }
+         Console.WriteLine("10 iters: " + sw.ElapsedMilliseconds);
+
+//         BenchmarkRunner.Run<HolePunch3DBenchmark>();
       }
    }
 
+
+   [Config(typeof(FastAndDirtyConfig))]
    public class HolePunch3DBenchmark {
-      private readonly TerrainService terrainService;
+      public readonly TerrainService terrainService;
 
       public HolePunch3DBenchmark() {
          var sectorGraphDescriptionStore = new SectorGraphDescriptionStore();
@@ -22,92 +46,112 @@ namespace OpenMOBA.Benchmark {
       }
 
       [Benchmark]
-      public void LoadBunny() {
-         terrainService.SnapshotCompiler.InvalidateCaches();
-         terrainService.Clear();
-         terrainService.LoadMeshAsMap("Assets/bunny.obj", new DoubleVector3(0.015, -0.10, 0.0), new DoubleVector3(0, 0, 0), 30000);
+      public void LoadBunny() => _ClearMapAndLoadBunny();
+
+      [Benchmark]
+      public void CompileBunny() {
+         _InvalidateCaches();
+         _CompileBunny();
       }
 
 
       [IterationSetup(Target = nameof(CompileBunny))]
-      public void CompileBunny_Setup() => LoadBunny();
+      void CompileBunny_Setup() => _ClearMapAndLoadBunny();
+
 
       [Benchmark]
-      public void CompileBunny() {
-         terrainService.CompileSnapshot().OverlayNetworkManager.CompileTerrainOverlayNetwork(15);
+      public void IncrementallyRecompileHolePunchedBunny() {
+         _PunchHoleIntoBunny();
+         _CompileBunny();
       }
 
       [IterationSetup(Target = nameof(IncrementallyRecompileHolePunchedBunny))]
       public void CompileHolePunchedBunny_Setup() {
-         LoadBunny();
-         CompileBunny();
+         _ClearMapAndLoadBunny();
+         _CompileBunny();
       }
 
-      [Benchmark]
-      public void IncrementallyRecompileHolePunchedBunny() {
-         PunchHoleIntoBunny();
-         CompileBunny();
+      public void _ClearMapAndLoadBunny() {
+         terrainService.Clear();
+         terrainService.LoadMeshAsMap("Assets/bunny.obj", new DoubleVector3(0.015, -0.10, 0.0), new DoubleVector3(0, 0, 0), 30000);
       }
 
-      public void PunchHoleIntoBunny() {
+      public void _InvalidateCaches() => terrainService.SnapshotCompiler.InvalidateCaches();
+
+      public void _CompileBunny() => terrainService.CompileSnapshot().OverlayNetworkManager.CompileTerrainOverlayNetwork(15);
+
+      public void _PunchHoleIntoBunny() {
          var sphereHole = terrainService.CreateHoleDescription(new SphereHoleStaticMetadata { Radius = 500 });
          sphereHole.WorldTransform = Matrix4x4.CreateTranslation(-561.450012207031f, -1316.31005859375f, -116.25f);
          terrainService.AddTemporaryHoleDescription(sphereHole);
       }
    }
 
-//
-//   public class GeometryBenchmark {
-//      private readonly Size mapDimensions;
-//      private readonly Polygon[] holePolygons;
-//
-//      public GeometryBenchmark() {
-//         mapDimensions = new Size(1000, 1000);
-//         var simpleHoles = new[] {
-//            Polygon.CreateRectXY(100, 100, 300, 300),
-//            Polygon.CreateRectXY(400, 200, 100, 100),
-//            Polygon.CreateRectXY(200, -50, 100, 150),
-//            Polygon.CreateRectXY(600, 600, 300, 300),
-//            Polygon.CreateRectXY(700, 500, 100, 100),
-//            Polygon.CreateRectXY(200, 700, 100, 100),
-//            Polygon.CreateRectXY(600, 100, 300, 50),
-//            Polygon.CreateRectXY(600, 150, 50, 200),
-//            Polygon.CreateRectXY(850, 150, 50, 200),
-//            Polygon.CreateRectXY(600, 350, 300, 50),
-//            Polygon.CreateRectXY(700, 200, 100, 100)
-//         };
-//
-//         var holeSquiggle = PolylineOperations.ExtrudePolygon(
-//            new[] {
-//               new IntVector2(100, 50),
-//               new IntVector2(100, 100),
-//               new IntVector2(200, 100),
-//               new IntVector2(200, 150),
-//               new IntVector2(200, 200),
-//               new IntVector2(400, 250),
-//               new IntVector2(200, 300),
-//               new IntVector2(400, 315),
-//               new IntVector2(200, 330),
-//               new IntVector2(210, 340),
-//               new IntVector2(220, 350),
-//               new IntVector2(220, 400),
-//               new IntVector2(221, 400)
-//            }.Select(iv => new IntVector2(iv.X + 160, iv.Y + 200)).ToArray(), 10).FlattenToPolygons();
-//         holePolygons = simpleHoles.Concat(holeSquiggle).ToArray();
-//
-//         hd = HoleDilation();
-//      }
-//
-////      [Benchmark]
-//      public List<Polygon> HoleDilation() {
-//         return PolygonOperations.Offset().Include(holePolygons).Dilate(15).Execute().FlattenToPolygons();
-//      }
-//
-//      private List<Polygon> hd;
-//
-//      [Benchmark]
-//      public void VisibilityGraph() {
-//         VisibilityGraphOperations.CreateVisibilityGraph(mapDimensions, hd);
-//      }
-//   }
+   public class FastAndDirtyConfig : ManualConfig {
+      public FastAndDirtyConfig() {
+         Add(DefaultConfig.Instance); // *** add default loggers, reporters etc? ***
+
+         Add(Job.Default
+                .WithLaunchCount(1) // benchmark process will be launched only once
+                .WithIterationTime(100 * TimeInterval.Millisecond) // 100ms per iteration
+                .WithWarmupCount(3) // 3 warmup iteration
+                .WithTargetCount(3) // 3 target iteration
+         );
+      }
+   }
+
+   //
+   //   public class GeometryBenchmark {
+   //      private readonly Size mapDimensions;
+   //      private readonly Polygon[] holePolygons;
+   //
+   //      public GeometryBenchmark() {
+   //         mapDimensions = new Size(1000, 1000);
+   //         var simpleHoles = new[] {
+   //            Polygon.CreateRectXY(100, 100, 300, 300),
+   //            Polygon.CreateRectXY(400, 200, 100, 100),
+   //            Polygon.CreateRectXY(200, -50, 100, 150),
+   //            Polygon.CreateRectXY(600, 600, 300, 300),
+   //            Polygon.CreateRectXY(700, 500, 100, 100),
+   //            Polygon.CreateRectXY(200, 700, 100, 100),
+   //            Polygon.CreateRectXY(600, 100, 300, 50),
+   //            Polygon.CreateRectXY(600, 150, 50, 200),
+   //            Polygon.CreateRectXY(850, 150, 50, 200),
+   //            Polygon.CreateRectXY(600, 350, 300, 50),
+   //            Polygon.CreateRectXY(700, 200, 100, 100)
+   //         };
+   //
+   //         var holeSquiggle = PolylineOperations.ExtrudePolygon(
+   //            new[] {
+   //               new IntVector2(100, 50),
+   //               new IntVector2(100, 100),
+   //               new IntVector2(200, 100),
+   //               new IntVector2(200, 150),
+   //               new IntVector2(200, 200),
+   //               new IntVector2(400, 250),
+   //               new IntVector2(200, 300),
+   //               new IntVector2(400, 315),
+   //               new IntVector2(200, 330),
+   //               new IntVector2(210, 340),
+   //               new IntVector2(220, 350),
+   //               new IntVector2(220, 400),
+   //               new IntVector2(221, 400)
+   //            }.Select(iv => new IntVector2(iv.X + 160, iv.Y + 200)).ToArray(), 10).FlattenToPolygons();
+   //         holePolygons = simpleHoles.Concat(holeSquiggle).ToArray();
+   //
+   //         hd = HoleDilation();
+   //      }
+   //
+   ////      [Benchmark]
+   //      public List<Polygon> HoleDilation() {
+   //         return PolygonOperations.Offset().Include(holePolygons).Dilate(15).Execute().FlattenToPolygons();
+   //      }
+   //
+   //      private List<Polygon> hd;
+   //
+   //      [Benchmark]
+   //      public void VisibilityGraph() {
+   //         VisibilityGraphOperations.CreateVisibilityGraph(mapDimensions, hd);
+   //      }
+   //   }
 }
