92e77f77afe321f53d773a85889a760603df06c3
Warty <ItzWarty@gmail.com>
2018-06-14 02:25:22 -0700

    use_fixed / use_double script wip

---
+++ scripts/use_fixed.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+replace="s/use_double/use_fixed/g"
+find "$DIR/.." -type f -name '*.csproj' -print0 | xargs -0 sed -i "$replace"
+find "$DIR/.." -type f -name '*.csproj' -print0 | xargs -0 unix2dos
