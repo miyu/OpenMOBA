f2438f3f5d56f75ae8ae9e71f1711513776157dc
Warty <ItzWarty@gmail.com>
2018-07-11 08:06:46 -0700

    Add files

---
+++ engine/src/OpenMOBA/Foundation/INetworkedSystem.cs
@@ -0,0 +1,5 @@
+namespace OpenMOBA.Foundation {
+   public interface INetworkedSystem {
+      object SaveState();
+   }
+}
---
+++ engine/src/OpenMOBA/Foundation/PeriodicStateSnapshotListener.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dargon.Vox;
+
+namespace OpenMOBA.Foundation {
+   public class PeriodicStateSnapshotListener : GameEventListener {
+      private readonly EntityWorld world;
+      private readonly InternalNetworkingLogService internalNetworkingLogService;
+      private readonly int periodicity;
+
+      public PeriodicStateSnapshotListener(EntityWorld world, InternalNetworkingLogService internalNetworkingLogService, int periodicity) {
+         this.world = world;
+         this.internalNetworkingLogService = internalNetworkingLogService;
+         this.periodicity = periodicity;
+      }
+
+      public override void HandleEnterTick(EnterTickStatistics statistics) {
+         if (statistics.Tick % periodicity != 0) return;
+         var snapshot = CaptureSnapshot();
+         internalNetworkingLogService.Add(snapshot);
+      } 
+
+      private PeriodicStateSnapshot CaptureSnapshot() {
+         var snapshot = new PeriodicStateSnapshot {
+            EntitySnapshots = new Dictionary<int, Dictionary<int, object>>(),
+            SystemSnapshots = new Dictionary<Type, object>()
+         };
+         foreach (var entity in world.EnumerateEntities()) {
+            var map = new Dictionary<int, object>();
+            for (var i = 0; i < entity.ComponentsByType.Length; i++) {
+               var component = entity.ComponentsByType[i];
+               if (!(component is INetworkedComponent nc)) continue;
+               map[i] = nc.SaveState();
+            }
+            snapshot.EntitySnapshots[entity.Id] = map;
+         }
+         foreach (var system in world.EnumerateSystems()) {
+            if (!(system is INetworkedSystem ns)) continue;
+            snapshot.SystemSnapshots[system.GetType()] = ns.SaveState();
+         }
+         return snapshot;
+      }
+   }
+
+   [AutoSerializable]
+   public class PeriodicStateSnapshot {
+      public Dictionary<int, Dictionary<int, object>> EntitySnapshots;
+      public Dictionary<Type, object> SystemSnapshots;
+   }
+}
